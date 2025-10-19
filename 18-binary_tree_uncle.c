5392a0646cd4abb0384ec172ef1d24894837c69d
ako11927 <11927@holbertonstudent.com>
2025-10-19 07:37:19 +0000

    Fix: Betty style (no assignment in if) for task 18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,16 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_uncle - Find uncle of a node
- * @node: node
- * Return: pointer to uncle or NULL
+ * binary_tree_uncle - Finds the uncle of a node
+ * @node: Pointer to the node to find the uncle of
+ *
+ * Return: Pointer to the uncle node, or NULL if it doesn't exist
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	binary_tree_t *p;
 
-	if (!node || !(p = node->parent) || !p->parent)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
+
+	p = node->parent;
+
 	if (p->parent->left == p)
 		return (p->parent->right);
 	return (p->parent->left);
